110f2649da1de6395f752855e9b5ab7337c4d2e3
Wexort <wexort.com@gmail.com>
2019-08-24 07:43:00 +0200

    fix rightTriangle

--- hello/RightTriangle.java
+++ hello/RightTriangle.java
@@ -3,28 +3,13 @@ public class RightTriangle {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
-        if (a <= 0 || b <= 0 || c <= 0) {
-            System.out.println("false");
-        } else {
-            int h, c1, c2;
-            if(a > b && a > c) {
-                h = a;
-                c1 = b;
-                c2 = c;
-            } else if (b > a && b > c) {
-                h = b;
-                c1 = a;
-                c2 = c;
-            }  else {
-                h = c;
-                c1 = a;
-                c2 = b;
-            }
-            if(Math.pow(h, 2) == Math.pow(c1, 2) + Math.pow(c2, 2)) {
-                System.out.println("true");
-            } else {
-                System.out.println("false");
-            }
-        }
+        
+        boolean isRightSlides = a > 0 && b > 0 && c > 0;
+        boolean isAHypotenuse = Math.pow(a, 2) == Math.pow(b, 2) + Math.pow(c, 2);      
+        boolean isBHypotenuse = Math.pow(b, 2) == Math.pow(c, 2) + Math.pow(a, 2);
+        boolean isCHypotenuse = Math.pow(c, 2) == Math.pow(a, 2) + Math.pow(b, 2);
+        
+        boolean isRightTriangle = isRightSlides && (isAHypotenuse || isBHypotenuse || isCHypotenuse);
+        System.out.println(isRightTriangle);
     }
 }
