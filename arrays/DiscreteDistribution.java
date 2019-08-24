f94292048ae2945fc20aa71e915e300bc96467a3
Wexort <wexort.com@gmail.com>
2019-08-24 20:07:11 +0200

    assignment 3.1 3.2 done

---
+++ arrays/DiscreteDistribution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class DiscreteDistribution {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int sum = 0;
+        int[] aSequence = new int[args.length-1];
+
+        for (int i = 1; i < args.length; i++) {
+            int a = Integer.parseInt(args[i]);
+            aSequence[i-1] = a;
+            sum += a;
+        }
+
+        int[] distribution = new int[sum];
+        int entry = 0;
+        for (int i = 0; i < aSequence.length; i++) {
+            for (int j = 0; j < aSequence[i]; j++) {
+                distribution[entry] = i + 1;
+                entry += 1;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            int r = (int) (Math.random() * sum);
+            System.out.print(distribution[r] + " ");
+        }
+        System.out.print('\n');
+    
+    }
+}
---
+++ arrays/ThueMorse.java
@@ -0,0 +1,27 @@
+
+public class ThueMorse {
+    public static void main(String[] args) {
+        int len = Integer.parseInt(args[0]);
+        boolean[] sequence = new boolean[len];
+
+        // construct ThueMorse sequence
+        for (int i = 1; i < len; i++) {
+            int p = 0;
+            while (i >= Math.pow(2, p)) {
+                p++;
+            }
+            p = p - 1;
+            int neg = i - (int) (Math.pow(2, p));
+            sequence[i] = !sequence[neg];  
+        }
+
+        // print out pattern
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (sequence[j] == sequence[i]) System.out.print("+ ");
+                else System.out.print("- ");
+            }
+            System.out.print('\n');
+        }
+    }
+}
