0d8575f2967639b37f91c9fceac76ad6db34c857
Wexort <wexort.com@gmail.com>
2019-08-24 12:58:23 +0200

    fix cast whitespace

--- hello/RightTriangle.java
+++ hello/RightTriangle.java
@@ -5,9 +5,9 @@ public class RightTriangle {
         int c = Integer.parseInt(args[2]);
         
         boolean isRightSlides = a > 0 && b > 0 && c > 0;
-        boolean isAHypotenuse = (int)Math.pow(a, 2) == (int)Math.pow(b, 2) + (int)Math.pow(c, 2);      
-        boolean isBHypotenuse = (int)Math.pow(b, 2) == (int)Math.pow(c, 2) + (int)Math.pow(a, 2);
-        boolean isCHypotenuse = (int)Math.pow(c, 2) == (int)Math.pow(a, 2) + (int)Math.pow(b, 2);
+        boolean isAHypotenuse = (int) Math.pow(a, 2) == (int) Math.pow(b, 2) + (int) Math.pow(c, 2);      
+        boolean isBHypotenuse = (int) Math.pow(b, 2) == (int) Math.pow(c, 2) + (int) Math.pow(a, 2);
+        boolean isCHypotenuse = (int) Math.pow(c, 2) == (int) Math.pow(a, 2) + (int) Math.pow(b, 2);
         
         boolean isRightTriangle = isRightSlides && (isAHypotenuse || isBHypotenuse || isCHypotenuse);
         System.out.println(isRightTriangle);
